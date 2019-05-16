d4943f7b62e1e6ae2472fd597a816a4c80161821
Administrator <guadan0711@126.com>
2019-05-16 16:10:48 +0800

    Initial Commit

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,173 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    avatarUrl: './user-unlogin.png',
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: '',
+    fontFamily: 'Bitstream Vera Serif Bold',
+    loaded: false,
+    imageUrl: "../../images/slogen.gif",
+    title: "",
+    content: "",
+    targetTime1: 0,
+    myFormat1: ['天', '时', '分', '秒'],
+    clearTimer: false
+  },
+
+  onLoad: function() {
+    var that = this
+    this.setData({
+      targetTime1: new Date().getTime() + 86430000,
+    });
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
+
+    wx.loadFontFace({
+      family: this.data.fontFamily,
+      source: 'url("https://sungd.github.io/Pacifico.ttf")',
+      success(res) {
+        that.setData({
+          loaded: true
+        })
+      },
+      fail: function(res) {
+        console.log(res.status)
+      },
+      complete: function(res) {
+        console.log(res.status)
+      }
+    });
+    //获取title图片及消息
+    // 1. 获取数据库引用
+    const db = wx.cloud.database()
+    // 2. 构造查询语句
+    // collection 方法获取一个集合的引用
+    // where 方法传入一个对象，数据库返回集合中字段等于指定值的 JSON 文档。API 也支持高级的查询条件（比如大于、小于、in 等），具体见文档查看支持列表
+    // get 方法会触发网络请求，往数据库取数据
+    db.collection('love')
+      .get({
+        success(res) {
+          var list = res.data
+          if (list.length > 0) {
+            console.log(list[0].imageUrl)
+            that.setData({
+              imageUrl: list[0].imageUrl,
+              title: list[0].title,
+              content: list[0].content,
+            })
+          }
+        }
+      })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+
+  onUnload() {
+    this.setData({
+      clearTimer: true
+    });
+  },
+  onGetUserInfo: function(e) {
+    if (!this.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo
+      })
+    }
+  },
+
+  onGetOpenid: function() {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+        wx.navigateTo({
+          url: '../userConsole/userConsole',
+        })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+        wx.navigateTo({
+          url: '../deployFunctions/deployFunctions',
+        })
+      }
+    })
+  },
+
+  // 上传图片
+  doUpload: function() {
+    // 选择图片
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: function(res) {
+
+        wx.showLoading({
+          title: '上传中',
+        })
+
+        const filePath = res.tempFilePaths[0]
+
+        // 上传图片
+        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
+        wx.cloud.uploadFile({
+          cloudPath,
+          filePath,
+          success: res => {
+            console.log('[上传文件] 成功：', res)
+
+            app.globalData.fileID = res.fileID
+            app.globalData.cloudPath = cloudPath
+            app.globalData.imagePath = filePath
+
+            wx.navigateTo({
+              url: '../storageConsole/storageConsole'
+            })
+          },
+          fail: e => {
+            console.error('[上传文件] 失败：', e)
+            wx.showToast({
+              icon: 'none',
+              title: '上传失败',
+            })
+          },
+          complete: () => {
+            wx.hideLoading()
+          }
+        })
+
+      },
+      fail: e => {
+        console.error(e)
+      }
+    })
+  },
+
+})
